ae1a007a6c7544c515afd6d47b1d2f37a4567f4f
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2023-06-05 23:23:58 -0300

    Rook - possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -46,9 +47,17 @@ public class ChessMatch {
         }
     }
 
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("The choosen piece can´t move to target position");
+        }
+    }
+
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
+
+
 
     private void initialSetup(){
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
